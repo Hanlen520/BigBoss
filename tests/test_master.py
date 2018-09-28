0ccab63c014c641f5aa7dbf7bd53594ece7108c0
williamfzc <178894043@qq.com>
2018-09-28 11:46:29 +0800

    add unittest case

--- tests/test_master.py
+++ tests/test_master.py
@@ -1,37 +1,48 @@
 import requests
 
 
-base_url = 'http://127.0.0.1:9507'
+# 本机测试
+base_master_url = 'http://127.0.0.1:9507'
+base_slaver_ip = '127.0.0.1'
 
 
 def test_ping():
-    resp = requests.get(base_url)
+    resp = requests.get(base_master_url)
     assert resp.ok
 
 
 def test_single_device_status():
-    target_url = base_url + '/api/device'
+    target_url = base_master_url + '/api/device'
     resp = requests.get(target_url, {
-        'target_ip': '127.0.0.1',
+        'target_ip': base_slaver_ip,
     })
     assert resp.ok
 
 
 def test_all_device_status():
-    target_url = base_url + '/api/device'
+    target_url = base_master_url + '/api/device'
     resp = requests.get(target_url)
     assert resp.ok
 
 
 def test_single_server_status():
-    target_url = base_url + '/api/slaver'
+    target_url = base_master_url + '/api/slaver'
     resp = requests.get(target_url, {
-        'target_ip': '127.0.0.1'
+        'target_ip': base_slaver_ip,
     })
     assert resp.ok
 
 
 def test_all_server_status():
-    target_url = base_url + '/api/slaver'
+    target_url = base_master_url + '/api/slaver'
     resp = requests.get(target_url)
+    assert resp.ok
+
+
+def test_exec_py():
+    target_url = base_master_url + '/api/task'
+    resp = requests.get(target_url, {
+        'script_name': 'hello.py',
+        'target_ip': base_slaver_ip,
+    })
     assert resp.ok
--- tests/test_slaver.py
+++ tests/test_slaver.py
@@ -1,22 +1,22 @@
 import requests
 
 
-base_url = 'http://127.0.0.1:9410'
+base_slaver_url = 'http://127.0.0.1:9410'
 
 
 def test_ping():
-    resp = requests.get(base_url)
+    resp = requests.get(base_slaver_url)
     assert resp.ok
 
 
 def test_device_status():
-    target_url = base_url + '/api/device/status'
+    target_url = base_slaver_url + '/api/device/status'
     resp = requests.get(target_url)
     assert resp.ok
 
 
 def test_device_command():
-    target_url = base_url + '/api/device/command'
+    target_url = base_slaver_url + '/api/device/command'
     resp = requests.get(target_url, {
         'adb_cmd': 'devices'
     })
